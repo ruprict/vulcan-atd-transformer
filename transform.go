60ffe6761f34b9abe1c1bf76ac5883dfecf9cc22
Glenn Goodrich <glenn.goodrich@gmail.com>
2016-10-06 11:24:16 -0400

    Add json writing ability

--- transform.go
+++ transform.go
@@ -1,12 +1,17 @@
 package transform
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"html/template"
+	"io"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/codegangsta/cli"
+	"github.com/vulcand/oxy/utils"
 	"github.com/vulcand/vulcand/plugin"
 )
 
@@ -34,10 +39,44 @@ type InventoryResponse struct {
 	EstimatedDeliveryDate time.Time
 }
 
+type bufferWriter struct {
+	header http.Header
+	code   int
+	buffer *bytes.Buffer
+}
+
+func (b *bufferWriter) Close() error {
+	return nil
+}
+
+func (b *bufferWriter) Header() http.Header {
+	return b.header
+}
+
+func (b *bufferWriter) Write(buf []byte) (int, error) {
+	return b.buffer.Write(buf)
+}
+
+// WriteHeader sets rw.Code.
+func (b *bufferWriter) WriteHeader(code int) {
+	b.code = code
+}
+
 func (h *TransformHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	json, _ := json.Marshal(InventoryResponse{4, time.Now()})
 	fmt.Println("*** atd_transformer middleware ***")
-	w.Write(json)
+
+	bw := &bufferWriter{header: make(http.Header), buffer: &bytes.Buffer{}}
+	newBody := bytes.NewBufferString("")
+	if err := applyString(string(json), newBody, r); err != nil {
+		fmt.Errorf("can't write bddy")
+		return
+	}
+	w.Header().Set("Content-Length", strconv.Itoa(newBody.Len()))
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	utils.CopyHeaders(w.Header(), bw.Header())
+	io.Copy(w, newBody)
 	return
 }
 
@@ -59,4 +98,22 @@ func FromCli(c *cli.Context) (plugin.Middleware, error) {
 
 func CliFlags() []cli.Flag {
 	return []cli.Flag{}
+}
+
+func applyString(in string, out io.Writer, request *http.Request) error {
+	t, err := template.New("t").Parse(in)
+	if err != nil {
+		return err
+	}
+
+	if err = t.Execute(out, data{request}); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// data represents template data that is available to use in templates.
+type data struct {
+	Request *http.Request
 }
